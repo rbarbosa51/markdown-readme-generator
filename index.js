27c4cf5f52632eef135b4f89d2e4325f034f0bd9
Rafael Barbosa <rafael.barbosa51@gmail.com>
2022-12-25 20:52:53 -0600

    In this commit: I created the backbone of the inquirer question answering system. At this stage, only title and description and bring asked.

--- index.js
+++ index.js
@@ -1,13 +1,39 @@
-// TODO: Include packages needed for this application
+//I am using modules (added "type":"modules" to package.json)
+import inquirer from "inquirer";
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    'What is the TITLE of this project ?',
+    'Enter a DESCRIPTION of this project: ',
+    'Enter INSTALLATION instructions: ',
+    'Enter USAGE Instructions: ',
+    'Enter CONTRIBUTION GUIDELINES: ',
+    'Enter TEST instructions',
+    'Choose a LICENSE for the application',
+    'Enter your GitHub username: ',
+    'Enter your email address'
+
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt([
+        {
+            name: 'title',
+            message: questions[0]
+        },
+        {
+            name: 'description',
+            message: questions[1]
+        }
+    ])
+    .then( (answers) => {
+        console.log(`Debug--`, answers);
+    });
+}
 
 // Function call to initialize app
 init();
