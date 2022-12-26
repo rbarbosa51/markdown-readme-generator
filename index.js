fa51cfcf2f4e2fbbef833b37bdf1e8d8edc8df36
Rafael Barbosa <rafael.barbosa51@gmail.com>
2022-12-25 23:16:13 -0600

    In this commit: Managed to create the license badges. and the app now creates README.md files

--- index.js
+++ index.js
@@ -20,8 +20,6 @@ const questions = [
 
 ];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
 function init() {
@@ -77,5 +75,5 @@ function init() {
     });
 }
 
-// Function call to initialize app
+// Start the app
 init();
--- template.js
+++ template.js
@@ -1,6 +1,29 @@
+const returnBadgeString = (license) => {
+    switch (license) {
+        case 'Apache 2.0 License':
+            return '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
+            break;
+        case 'GNU GPL v3':
+            return '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)';
+            break;
+        case 'MIT License':
+            return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+            break;
+        case 'Mozilla Public License 2.0':
+            return '[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)';
+            break;
+        default:
+            //CC0
+            return '[![License: CC0-1.0](https://licensebuttons.net/l/zero/1.0/80x15.png)](http://creativecommons.org/publicdomain/zero/1.0/)';
+            break;
+    }
+}
 const createReadmeTemplate = (answers) => {
+    const badge = returnBadgeString(answers.license);
     let template = 
 `# ${answers.title}
+
+${badge}
 
 ## Description
 
